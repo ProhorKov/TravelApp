37495f2f2ab6210ef322db8b56995d00bc807b5d
ProhorKov <prohor.stud@yandex.ru>
2023-11-17 01:36:49 +0300

    Editing the database schema
    Editing the db/migrate/20231116194133_create_users.rb file for the requirements of the task

--- db/migrate/20231116194133_create_users.rb
+++ db/migrate/20231116194133_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
-      t.string :first_name
-      t.string :last_name
-      t.string :email
+      t.string :first_name, null: false, limit: 100
+      t.string :last_name, null: false, limit: 100
+      t.string :email, null: false, limit: 150
 
       t.timestamps
     end
