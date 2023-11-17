4f5c355ea256a05db9729a8bab791a9084652881
Prohor Kovalchuk <prohor.stud@yandex.ru>
2023-11-17 19:09:50 +0300

    HomeController Update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,16 @@
+# app/controllers/home_controller.rb
 class HomeController < ApplicationController
+  before_action :authenticate_user!, only: [:dashboard]
+
   def index
+    if user_signed_in?
+      redirect_to edit_user_registration_path
+    else
+      # Страница, отображаемая для незалогиненных пользователей
+    end
   end
 
   def dashboard
+    # Содержимое страницы для залогиненных пользователей
   end
 end
